d67e8f6d935d9c9953213cfff1d19b347adc6889
Ian Brandão <ianbrandao_@hotmail.com>
2023-08-31 00:36:40 -0300

    f

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/src/index.tsx*',
+        destination: 'https://trident-server.vercel.app/admin-updates*',
+      },
+    ]
+  },
+}
